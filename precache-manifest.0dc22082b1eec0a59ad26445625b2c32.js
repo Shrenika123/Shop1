ed7d08557a79883b1a4fa2afe41508fa5cc1bbff
“Shrenika123” <shrenika.nataraj@gmail.com>
2020-10-07 12:02:20 +0530

    Updates

---
+++ precache-manifest.0dc22082b1eec0a59ad26445625b2c32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe262a3367fc8db7a41d5fe1cddef6d",
+    "url": "/Shop1/index.html"
+  },
+  {
+    "revision": "074a52b5e8d499f8e631",
+    "url": "/Shop1/static/css/3.ed0685cc.chunk.css"
+  },
+  {
+    "revision": "b0b3520bed21d477bd09",
+    "url": "/Shop1/static/css/main.d689ce10.chunk.css"
+  },
+  {
+    "revision": "eefdc7cf4bfa812deb0b",
+    "url": "/Shop1/static/js/2.8c91fc91.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Shop1/static/js/2.8c91fc91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074a52b5e8d499f8e631",
+    "url": "/Shop1/static/js/3.a5283572.chunk.js"
+  },
+  {
+    "revision": "b0b3520bed21d477bd09",
+    "url": "/Shop1/static/js/main.3afa9abf.chunk.js"
+  },
+  {
+    "revision": "a312a4ddd23c22ade9b2",
+    "url": "/Shop1/static/js/runtime-main.5395a6ce.js"
+  }
+]);
